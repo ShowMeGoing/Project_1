275debbc859cc28f1e845ebc54de3be548cd120d
Hao Zhi <thzaloysius@gmail.com>
2024-08-16 09:58:33 +0800

    Update OOP

---
+++ Packages/Tasks.py
@@ -0,0 +1,8 @@
+class Tasks:
+    def __init__(self, name, deadline):
+        self.name =  name
+        self.deadline = deadline
+
+
+def Greet():
+    print("Moo")
--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print("Hello World")
+from Packages import Tasks as tasks
+
+tasks.Greet()
+
